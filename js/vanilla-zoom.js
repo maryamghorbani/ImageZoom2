76c9a5715b7759cb9c1b4ac2f34590880cd9b102
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-21 00:04:04 +0430

    make zoom lens

--- js/vanilla-zoom.js
+++ js/vanilla-zoom.js
@@ -22,8 +22,26 @@
                 zoomContainer.style.left = `${imageBox.left + window.pageXOffset}px`
 
                 zoomLens = document.createElement('div')
+                zoomLens.classList.add('img-zoom-lens')
+                zoomContainer.insertAdjacentElement('afterbegin', zoomLens)
 
                 document.querySelector('body').insertAdjacentElement('beforeend', zoomContainer);
+
+                zoomContainer.addEventListener('mousemove', function (e) {
+                    let x = e.clientX - imageBox.left;
+                    let y = e.clientY - imageBox.top;
+
+                    x = x - (zoomLens.offsetWidth / 2)
+                    y = y - (zoomLens.offsetHeight / 2)
+
+                    if (x > image.width - zoomLens.offsetWidth) {x = image.width - zoomLens.offsetWidth}
+                    if (x < 0) {x = 0}
+                    if (y > image.height - zoomLens.offsetHeight) {y = image.height - zoomLens.offsetHeight}
+                    if (y < 0) {y = 0}
+
+                    zoomLens.style.top = `${y}px`;
+                    zoomLens.style.left = `${x}px`;
+                })
 
 
             })
