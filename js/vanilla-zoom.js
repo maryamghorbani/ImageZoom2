48ce65fc9b750ee784e3e877d711922c019a129a
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-20 20:42:06 +0430

    create zoom container

--- js/vanilla-zoom.js
+++ js/vanilla-zoom.js
@@ -2,7 +2,28 @@
 
     let defineLibrary = () => ({
         init: function (imageId) {
-            console.log(imageId);
+            let image = document.querySelector(imageId)
+            if (! image) {
+                console.error(`Image element doesn't exist`);
+                return;
+            }
+
+            let zoomContainer, zoomResult, zoomLens;
+
+            image.addEventListener('mouseenter', function (e) {
+                let imageBox = this.getBoundingClientRect();
+
+                zoomContainer = document.createElement('div');
+                zoomContainer.classList.add('zoom-container');
+                zoomContainer.style.width = `${imageBox.width}px`
+                zoomContainer.style.height = `${imageBox.height}px`
+                
+                document.querySelector('body').insertAdjacentElement('beforeend', zoomContainer);
+                zoomContainer.style.position = 'absolute';
+                zoomContainer.style.top = `${imageBox.top + window.pageYOffset}px`
+                zoomContainer.style.left = `${imageBox.left + window.pageXOffset}px`
+
+            })
         }
     })
 
